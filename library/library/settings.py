b0b82b004ee71a73278879a7cec849fb85a07dd6
out-ofsight <73275046+out-ofsight@users.noreply.github.com>
2020-11-03 11:32:27 +0200

    .

--- library/library/settings.py
+++ library/library/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'start_menu.apps.StartMenuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
